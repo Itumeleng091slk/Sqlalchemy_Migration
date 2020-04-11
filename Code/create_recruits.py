0d03009bc216094234152acc1e39ff0a91be8699
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-04-11 13:03:03 +0200

    Update create_recruits.py

--- Code/create_recruits.py
+++ Code/create_recruits.py
@@ -31,7 +31,7 @@ class Recruit(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     first_name = db.Column(db.String(20))
     surname = db.Column(db.String(20))
-    personal_email_address = db.Column(db.VARCHAR(100))
+    personal_email_address = db.Column(db.VARCHAR(100), unique=True)
     chat_name = db.Column(db.VARCHAR(50))
     github_name = db.Column(db.VARCHAR(50))
     id_number = db.Column(db.NUMERIC(15))
